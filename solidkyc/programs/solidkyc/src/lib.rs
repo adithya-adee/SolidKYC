f5a423c3e820570822fc52a82a86b4ef146926d5
adithya-adee <adithya25905@gmail.com>
2025-11-29 00:54:22 +0530

    Implement account(Issuer Registry) , smart contract and test for intialize_issuer_registry

--- solidkyc/programs/solidkyc/src/lib.rs
+++ solidkyc/programs/solidkyc/src/lib.rs
@@ -16,6 +16,17 @@ pub mod solidkyc {
         msg!("SolidKYC initialized with admin: {}", config.admin);
         Ok(())
     }
+
+    pub fn initialize_issuer_registry(ctx: Context<InitializeIssuerRegistry>) -> Result<()> {
+        let issuer_registry = &mut ctx.accounts.issuer_registry;
+
+        issuer_registry.admin = ctx.accounts.admin.key();
+        issuer_registry.count = 0;
+        issuer_registry.bump = ctx.bumps.issuer_registry;
+
+        msg!("Issuer registry initialized");
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -35,6 +46,32 @@ pub struct InitializeProgramConfig<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct InitializeIssuerRegistry<'info> {
+    #[account(
+        seeds = [b"config"],
+        bump
+    )]
+    pub config: Account<'info, ProgramConfig>,
+
+    #[account(
+        init,
+        payer = admin,
+        space = IssuerRegistry::SIZE,
+        seeds = [b"issuer_registry"],
+        bump
+    )]
+    pub issuer_registry: Box<Account<'info, IssuerRegistry>>,
+
+    #[account(
+        mut,
+        address = config.admin @ SolidKycError::UnauthorizedAdmin
+    )]
+    pub admin: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct ProgramConfig {
     pub admin: Pubkey,
@@ -44,4 +81,22 @@ pub struct ProgramConfig {
 
 impl ProgramConfig {
     pub const SIZE: usize = 8 + 32 + 1 + 1; // 8(discriminator) + 34 = 42 bytes total
+}
+
+#[account]
+pub struct IssuerRegistry {
+    pub admin: Pubkey,
+    pub issuers: [Pubkey; 32],
+    pub count: u8,
+    pub bump: u8,
+}
+
+impl IssuerRegistry {
+    pub const SIZE: usize = 8 + 32 + 32 * 32 + 1 + 1; // 8 (discriminator) + 1058 = 1066 bytes total
+}
+
+#[error_code]
+pub enum SolidKycError {
+    #[msg("Unauthorized: Only admin can perform this action")]
+    UnauthorizedAdmin,
 }
