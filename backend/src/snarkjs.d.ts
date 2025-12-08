a635405af5b0387beb89ca5367bbd3970004b739
Adithya A <144262826+adithya-adee@users.noreply.github.com>
2025-12-08 21:45:09 +0530

    Updated Backend with issue credentials (#4)
    * Typescript + Snarkjs backend
    
    * Implement issuer crendential route
    
    * Add public.json and proof.json to gitignore and upate the verfication key for a updated circom circuit

---
+++ backend/src/snarkjs.d.ts
@@ -0,0 +1,15 @@
+declare module 'snarkjs' {
+  export namespace groth16 {
+    function verify(
+      vKey: any,
+      publicSignals: string[],
+      proof: any
+    ): Promise<boolean>;
+    
+    function fullProve(
+      input: any,
+      wasmFile: string,
+      zkeyFile: string
+    ): Promise<{ proof: any; publicSignals: string[] }>;
+  }
+}
